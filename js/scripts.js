ce81b52989797d9b0048d254f5a7aad3564be609
Samantha Andrews <samantharaeandrews@gmail.com>
2020-10-25 15:34:15 -0600

    cleanup and make the circle text actually look nice

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
+  if (themeName === "theme-party") {
+    document.getElementById("logo").src = "assets/logo_party.png";
+    document.getElementById("header-image").src = "assets/cookie_kit_ingredients.png";
+    document.getElementById("pizza-kit-image").src = "assets/disco_ball.png";
+  } else {
+    document.getElementById("logo").src = "assets/logo_pizza.png";
+    document.getElementById("header-image").src = "assets/pizza_kit_ingredients.png";
+    document.getElementById("pizza-kit-image").src = "assets/pizza_slice.png";
+  }
   localStorage.setItem("theme", themeName);
   document.documentElement.className = themeName;
 }
+
+function setChecked(themeName) {
+  if (themeName === "theme-pizza") {
+    document.getElementById("toggle-input").checked = false;
+  } else document.getElementById("toggle-input").checked = true;
+}
+
 // function to toggle between pizza and party theme
 function toggleTheme() {
   if (localStorage.getItem("theme") === "theme-pizza") {
@@ -15,24 +31,15 @@ function toggleTheme() {
 (function () {
   if (localStorage.getItem("theme") === "theme-pizza") {
     setTheme("theme-pizza");
+    setChecked("theme-pizza");
   } else {
     setTheme("theme-party");
+    setChecked("theme-party");
   }
 })();
 
-const radius = document.body.clientWidth - 100;
-
-console.log(radius);
-
-const circleText = new CircleType(document.getElementById("circle-text")).radius(radius);
-const reverseCircleText = new CircleType(document.getElementById("reverse-circle-text")).dir(-1).radius(radius);
-
-// window.addEventListener("resize", function updateRadius() {
-//   circleText.radius(circleText.element.offsetWidth / 2);
-//   reverseCircleText.radius(circleText.element.offsetWidth / 2);
-// });
-// updateRadius();
+const circleText = new CircleType(document.getElementById("circle-text")).radius(200);
+const reverseCircleText = new CircleType(document.getElementById("reverse-circle-text")).dir(-1).radius(200);
 
 window.fitText(document.getElementById("circle-text"));
-
 window.fitText(document.getElementById("reverse-circle-text"));
